8cb8bc0d09f885ed29f453cbb6ac9d71ba0d1a0d
Roarco <sebuscaroberth@gmail.com>
2021-11-01 21:25:08 -0500

    GET:parametros query

--- index.js
+++ index.js
@@ -1,25 +1,152 @@
-//requerimos express
+//requerimos express y faker
 const express = require('express');
+const faker = require('faker');
 const app = express();
-const port = 3000;
+const port = 4000;
 
-app.get('/', (req, res) =>  {
-    res.send('Hello World!');
-});
-app.get('/creador', (req, res) =>  {
-  res.json({
-    nombre: 'Juan',
-    apellido: 'Perez',
-    edad: '30',
-    perfil: "Software Engineering is "
-})
-});
+/* products */
+
+//GET
+
 app.get('/products', (req, res) =>  {
-  res.json({
-  name: 'Product 1',
-  price: '$100',
-  description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-})
+  const products = [];
+  const { zise } = req.query;
+  const limit = zise || 10;
+  for (let i = 0; i < limit; i++) {
+    products.push({
+      id: i,
+      name: faker.commerce.productName(),
+      price: parseInt(faker.commerce.price(),10),
+      image: faker.image.image()
+    });
+  }
+  res.json(products);
 });
+
+app.get('/products/:id', (req, res) =>  {
+  const { id } = req.params.id;
+  res.json({
+    id,
+    name: 'Producto 1',
+    price: '$100'
+  })
+})
+
+//POST
+app.post('/products', (req, res) =>  {
+  res.json({
+    id: 1,
+    name: 'Producto 1',
+    price: '$100'
+  })
+})
+
+//PUT
+app.put('/products/:id', (req, res) =>  {
+  const { id } = req.params.id;
+  res.json({
+    id,
+    name: 'Producto 1',
+    price: '$100'
+  })
+})
+
+//DELETE
+app.delete('/products/:id', (req, res) =>  {
+  const { id } = req.params.id;
+  res.json({
+    id,
+    name: 'Producto 1',
+    price: '$100'
+  })
+})
+
+
+/* users */
+
+//GET
+app.get('/users', (req, res) =>  {
+  const {limit, offset} = req.query;
+  if(limit && offset){
+    res.json({
+      limit,
+      offset
+    })
+  }else{
+    res.send('limit and offset are required')
+  }
+})
+
+//POST
+app.post('/users', (req, res) =>  {
+  res.json({
+    id: 1,
+    name: 'User 1',
+    email: 'example@gmail.com',
+    password: '123456'
+  })
+})
+
+/* auth */
+
+//GET
+app.get('/auth/profile', (req, res) =>  {
+  res.json({
+    token: '123456'
+  })
+})
+
+//POST
+app.post('/auth/login', (req, res) =>  {
+  res.json({
+    token: '123456'
+  })
+})
+
+/* categories */
+
+//GET
+app.get('/categories', (req, res) =>  {
+  res.json([
+    {
+      id: 1,
+      name: 'Categoria 1'
+    },
+    {
+      id: 2,
+      name: 'Categoria 2'
+    },
+    {
+      id: 3,
+      name: 'Categoria 3'
+    }
+  ])
+})
+
+app.get('/categories/:id/products', (req, res) => {
+  const { id } = req.params;
+  res.json([
+    {
+      id,
+    },
+  ]);
+})
+
+/* files */
+
+//POST
+app.post('/files/upload', (req, res) =>  {
+  res.json({
+    id: 1,
+    name: 'File 1',
+    url: 'http://example.com/file1.jpg'
+  })
+})
+
+//GET
+app.get('/files/:filename', (req, res) =>  {
+  const { filename } = req.params;
+  res.sendFile(`${__dirname}/files/${filename}`);
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
