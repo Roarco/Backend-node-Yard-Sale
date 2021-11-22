32b30431c6916119c472551a683118c332ff9796
Roarco <sebuscaroberth@gmail.com>
2021-11-21 21:30:12 -0500

    Deploy en Heroku

--- app/libs/postgres.pool.js
+++ app/libs/postgres.pool.js
@@ -2,9 +2,15 @@
 const { Pool } = require('pg');
 const config = require('../config/config');
 
-const USER = encodeURIComponent(config.dbUser);
-const PASSWORD = encodeURIComponent(config.dbPassword);
-const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+let URI = '';
+
+if(config.isProd){
+  URI = config.dbUrl;  // URI = 'postgres://user:pass@localhost:5432/dbname';
+}else{
+  const USER = encodeURIComponent(config.dbUser);
+  const PASSWORD = encodeURIComponent(config.dbPassword);
+  URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+}
 
   const pool = new Pool({
     connectionString: URI,
